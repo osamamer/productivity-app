72c23355204567520ed28143bcdf930eb3b75789
osama <commercialcomedy02@gmail.com>
2023-09-29 14:02:00 +0300

    changed order + fixed duplication

--- frontend/main.js
+++ frontend/main.js
@@ -5,66 +5,26 @@ window.onload = async function() {
     let taskElements = await fetchTasks();
     displayTasks(taskElements);
 }
-
-function getStartSessionButtonId(taskId) {
-    return `start-session-button-${taskId}`
-}
-function getEndSessionButtonId(taskId) {
-    return `end-session-button-${taskId}`
-}
-
-function createStartSessionButton(taskJson) {
-    const button = document.createElement("button");
-    const taskId = taskJson["taskId"];
-    button.textContent = "Start session";
-    button.setAttribute("id", getStartSessionButtonId(taskId));
-    button.onclick = async (ev) => {
-        await startTaskSession(taskId);
-        button.setAttribute("style", "display: none");
-        document.getElementById(getEndSessionButtonId(taskId)).setAttribute("style", "display: block");
-    }
-    return button;
-}
-
-function createEndSessionButton(taskJson) {
-    const button = document.createElement("button");
-    button.setAttribute("style", "display: none");
-    button.textContent = "End session";
-    const taskId = taskJson["taskId"];
-    button.setAttribute("id", getEndSessionButtonId(taskId));
-    button.onclick = async (ev) => {
-        await endTaskSession(taskId);
-        button.setAttribute("style", "display: none");
-        document.getElementById(getStartSessionButtonId(taskId)).setAttribute("style", "display: block");
-    }
-    return button;
-}
-
-function createTaskElement(taskJson) {
-    const taskDiv =  document.createElement("div");
-    const taskHeader = document.createElement("h5");
-    taskHeader.textContent = taskJson["name"];
-    taskDiv.appendChild(taskHeader);
-    taskDiv.appendChild(createStartSessionButton(taskJson));
-    taskDiv.appendChild(createEndSessionButton(taskJson));
-    return taskDiv;
-}
-
 async function fetchTasks () {
     const response = await fetch('http://localhost:8080/api/v1/task');
     const responseJson = await response.json();
     let taskElements = [];
     for (let i = 0; i < responseJson.length; i++) {
         let taskElement = createTaskElement(responseJson[i]);
-        taskElements.push(taskElement); // `` makes something into a string
+        taskElements.push(taskElement);
     }
     return taskElements;
 }
-
- async function createNewTask (){
+function displayTasks (taskElements) {
+    document.getElementById("all-tasks-div").innerHTML = "";
+    for (let i = 0; i < taskElements.length; i++) {
+        document.getElementById("all-tasks-div").appendChild(taskElements[i]);
+    }
+}
+async function createNewTask (){
     //e.preventDefault();
     const userTaskName = document.getElementById("task-input-field").value;
-     document.getElementById("task-input-field").value = "";
+    document.getElementById("task-input-field").value = "";
     console.log(userTaskName);
     await fetch(TASK_URL, {
         method: "POST",
@@ -79,12 +39,69 @@ async function fetchTasks () {
         .then(() => fetchTasks())
         .then((tasksString) => displayTasks(tasksString))
 }
- function displayTasks (taskElements) {
-     for (let i = 0; i < taskElements.length; i++) {
-         document.getElementById("all-tasks-div").appendChild(taskElements[i]);
-     }
+function createTaskElement(taskJson) {
+    const taskDiv =  document.createElement("div");
+    taskDiv.classList.add("task-div");
+    const taskHeader = document.createElement("p");
+    taskHeader.textContent = taskJson["name"];
+    taskDiv.appendChild(taskHeader);
+    taskDiv.appendChild(createStartSessionButton(taskJson));
+    taskDiv.appendChild(createEndSessionButton(taskJson));
+    return taskDiv;
 }
 
+function createStartSessionButton(taskJson) {
+    const button = document.createElement("img");
+    button.classList.add("start-task-button");
+    const taskId = taskJson["taskId"];
+    button.textContent = "Start session";
+    button.setAttribute("id", getStartSessionButtonId(taskId));
+    button.onclick = async (ev) => {
+        await startTaskSession(taskId);
+        button.setAttribute("style", "display: none");
+        document.getElementById(getEndSessionButtonId(taskId)).setAttribute("style", "display: block");
+    }
+    return button;
+}
+function createEndSessionButton(taskJson) {
+    const button = document.createElement("img");
+    button.classList.add("end-task-button");
+    button.setAttribute("style", "display: none");
+    button.textContent = "End session";
+    const taskId = taskJson["taskId"];
+    button.setAttribute("id", getEndSessionButtonId(taskId));
+    button.onclick = async (ev) => {
+        await endTaskSession(taskId);
+        button.setAttribute("style", "display: none");
+        document.getElementById(getStartSessionButtonId(taskId)).setAttribute("style", "display: block");
+    }
+    return button;
+}
+function getStartSessionButtonId(taskId) {
+    return `start-session-button-${taskId}`
+}
+function getEndSessionButtonId(taskId) {
+    return `end-session-button-${taskId}`
+}
+async function performTaskAction(taskId, action) {
+    await fetch(TASK_URL.concat(`/${action}/${taskId}`), { // `` makes something into a string
+        method: "POST",
+    })
+}
+async function startTaskSession(taskId) {
+    await performTaskAction(taskId, "start-session");
+}
+
+async function endTaskSession(taskId) {
+    await performTaskAction(taskId, "end-session");
+}
+
+
+
+
+
+
+// FUNCTIONS BELOW ARE DEPRECATED!
 async function createNewTaskButton (){
     await fetch(TASK_URL, {
         method: "POST",
@@ -100,15 +117,4 @@ async function createNewTaskButton (){
         .then((tasksString) => displayTasks(tasksString))
 }
 
-async function startTaskSession(taskId) {
-    await performTaskAction(taskId, "start-session");
-}
 
-async function endTaskSession(taskId) {
-    await performTaskAction(taskId, "end-session");
-}
-async function performTaskAction(taskId, action) {
-    await fetch(TASK_URL.concat(`/${action}/${taskId}`), {
-        method: "POST",
-    })
-}
