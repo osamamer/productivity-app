099ec199315f0dc255832d39072a80b0bb1d90c3
osama <commercialcomedy02@gmail.com>
2023-10-04 19:55:38 +0300

    with basic task highlight box

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,16 @@ const PAUSE_IMG = "images/pause.png";
 const DELETE_IMG = "images/close.png";
 const DOTS_IMG = "images/dots.png";
 
+const inputField=document.getElementById("task-input-field");
+
+function stopFormSubmit(event) {
+    console.log("Trying to prevent default");
+    event.preventDefault();
+}
+inputField.addEventListener('submit', function (){
+    stopFormSubmit(event);
+    createNewTask();
+});
 
 window.onload = async function() {
     let taskElements = await fetchTasks();
@@ -14,6 +24,7 @@ async function fetchTasks () {
     const response = await fetch('http://localhost:8080/api/v1/task');
     const responseJson = await response.json();
     let taskElements = [];
+    if (responseJson.length === 0) return taskElements;
     for (let i = 0; i < responseJson.length; i++) {
         let taskElement = await createTaskElement(responseJson[i]);
         taskElements.push(taskElement);
@@ -29,9 +40,9 @@ function displayTasks (taskElements) {
 async function createNewTask (){
     //e.preventDefault();
     const userTaskName = document.getElementById("task-input-field").value;
-    if (userTaskName === "") return;
+    if (userTaskName.trim() === "") return;
     document.getElementById("task-input-field").value = "";
-    console.log(userTaskName);
+    console.log("Creating task: " + userTaskName);
     await fetch(TASK_URL, {
         method: "POST",
         body: JSON.stringify({
@@ -45,6 +56,19 @@ async function createNewTask (){
         .then(() => fetchTasks())
         .then((tasksString) => displayTasks(tasksString))
 }
+
+function highlightTask(taskJson) {
+    const highlightedTaskDiv = document.getElementById("highlighted-task-div");
+    while (highlightedTaskDiv.firstChild) {
+        highlightedTaskDiv.removeChild(highlightedTaskDiv.lastChild);
+    }
+    highlightedTaskDiv.setAttribute("style", "display: block");
+    const highLightedTaskHeader = document.createElement("div");
+    highlightedTaskDiv.appendChild(highLightedTaskHeader);
+    highLightedTaskHeader.classList.add("task-text");
+    highLightedTaskHeader.textContent = taskJson["name"];
+}
+
 async function createTaskElement(taskJson) {
     const taskDiv = document.createElement("div");
     const taskHeader = document.createElement("p");
@@ -58,6 +82,9 @@ async function createTaskElement(taskJson) {
     taskDiv.appendChild(startButton);
     taskDiv.appendChild(endButton);
     taskDiv.appendChild(createDeleteTaskButton(taskJson));
+    taskDiv.addEventListener('click', function(){
+        highlightTask(taskJson);
+    });
     return taskDiv;
 }
 function createTaskActionButton(action, taskJson, idSupplier, otherButtonIdSupplier, sessionFunction, buttonImage) {
@@ -69,7 +96,7 @@ function createTaskActionButton(action, taskJson, idSupplier, otherButtonIdSuppl
     const taskId = taskJson["taskId"];
     button.textContent = `${action} session`;
     button.setAttribute("id", idSupplier(taskId));
-    button.onclick = async (ev) => {
+    button.onclick = async () => {
         await sessionFunction(taskId);
         button.setAttribute("style", "display: none");
         document.getElementById(otherButtonIdSupplier(taskId)).setAttribute("style", "display: block");
@@ -139,9 +166,7 @@ async function getTaskRunning(taskId) {
     let promise = await fetch(TASK_URL.concat(`/get-task-running/${taskId}`), { // `` makes something into a string
         method: "POST",
     })
-    let promiseResponse = await promise.json();
-    console.log("is active: " + promiseResponse);
-    return promiseResponse;
+    return await promise.json();
 }
 
 async function deleteTask(taskId) {
@@ -158,23 +183,38 @@ async function deleteTask(taskId) {
 // 2. Can enter an empty task. FIXED
 // 3. When you delete all tasks, there is an exception.
 // 4. Accumulated Time is only updated when you end a session.
+// 5. onsubmit through JS doesn't work.
+// 6. Scrolling down to bottom of box crops last task a bit.
 
 
-// FUNCTIONS BELOW ARE DEPRECATED!
-async function createNewTaskButton() {
-    await fetch(TASK_URL, {
-        method: "POST",
-        body: JSON.stringify({
-            taskName: "userTaskName",
-            taskDescription: "A user submitted task"
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-        .then(() => fetchTasks())
-        .then((tasksString) => displayTasks(tasksString))
-}
-
-
+// function timer() {
+//
+// }
+// let time = setInterval(updateStopwatch, 1000);
+//
+// let startTime; // to keep track of the start time
+// let stopwatchInterval; // to keep track of the interval
+// let elapsedPausedTime = 0;
+// function startStopwatch() {
+//     let startTime = Date.now();
+//     setInterval(updateStopwatch, 1000, startTime);
+// }
+// function stopStopwatch(stopwatchInterval, startTime) {
+//     clearInterval(stopwatchInterval); // stop the interval
+//     elapsedPausedTime = new Date().getTime() - startTime; // calculate elapsed paused time
+//     stopwatchInterval = null; // reset the interval variable
+// }
+// function updateStopwatch(startTime) {
+//     function pad(number) {
+//         // add a leading zero if the number is less than 10
+//         return (number < 10 ? "0" : "") + number;
+//     }
+//     const currentTime = new Date().getTime(); // get current time in milliseconds
+//     const elapsedTime = currentTime - startTime; // calculate elapsed time in milliseconds
+//     const seconds = Math.floor(elapsedTime / 1000) % 60; // calculate seconds
+//     const minutes = Math.floor(elapsedTime / 1000 / 60) % 60; // calculate minutes
+//     const hours = Math.floor(elapsedTime / 1000 / 60 / 60); // calculate hours
+//      // format display time
+//     document.getElementById("stopwatch").innerHTML = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds); // update the display
+// }
 
