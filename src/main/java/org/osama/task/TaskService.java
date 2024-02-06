c8a274d77caedeb75af23a24ea05dc941bf594f6
osama <commercialcomedy02@gmail.com>
2024-02-07 02:20:35 +0300

    first linux commit

--- src/main/java/org/osama/task/TaskService.java
+++ src/main/java/org/osama/task/TaskService.java
@@ -4,11 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.osama.session.Session;
 import org.osama.session.SessionRepository;
 import org.springframework.stereotype.Service;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
