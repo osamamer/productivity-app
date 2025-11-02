bd9b11bf2a2fe2bbbdd437362fa3ad685d6f2e14
osama <commercialcomedy02@gmail.com>
2025-11-02 19:27:53 +0300

    - no postgres by default

--- backend/src/main/java/org/osama/SpringBootApp.java
+++ backend/src/main/java/org/osama/SpringBootApp.java
@@ -1,23 +1,13 @@
 package org.osama;
 
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Date;
-import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
 public class SpringBootApp {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SpringBootApp.class);
-        app.setAdditionalProfiles("postgres");
-        app.run(args);
+        new SpringApplication(SpringBootApp.class).run(args);
     }
 }
